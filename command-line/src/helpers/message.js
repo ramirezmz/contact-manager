9c85bf2dad5779ad6ca0c7cdca586fea2bfb9ef4
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2023-04-08 18:13:27 -0300

    🌟feat: add menu and register question

--- command-line/src/helpers/message.js
+++ command-line/src/helpers/message.js
@@ -1,6 +1,8 @@
 import { sleep } from '../utils/sleep.js'
+// import { spinner } from '../utils/spinner.js'
 import chalk from 'chalk'
 import chalkAnimation from 'chalk-animation'
+import inquirer from 'inquirer'
 
 async function welcome () {
   const rainbowTitle = chalkAnimation.rainbow(
@@ -20,6 +22,88 @@ async function welcome () {
   `)
 }
 
+async function menu () {
+  const answers = await inquirer.prompt({
+    type: 'list',
+    name: 'menu',
+    message: 'What do you want to do?',
+    choices: [
+      'Login to your account',
+      'Create an account',
+      'Exit',
+      'Sudo mode'
+    ]
+  })
+  if (answers.menu === 'Login to your account') {
+    console.log('Loginnnn')
+  }
+  if (answers.menu === 'Create an account') {
+    console.log('Create an account')
+    await register()
+  }
+  if (answers.menu === 'Exit') {
+    process.exit()
+  }
+  if (answers.menu === 'Sudo mode') {
+    console.log('Sudo mode activated')
+  }
+}
+const newUser = {}
+async function register () {
+  const answers = await inquirer.prompt([
+    {
+      name: 'name',
+      type: 'input',
+      message: 'What is your name?',
+      default () {
+        return 'Player'
+      }
+    },
+    {
+      name: 'username',
+      type: 'input',
+      message: 'What will be your username?',
+      default () {
+        return 'Player-wins'
+      }
+    },
+    {
+      name: 'password',
+      type: 'password',
+      message: 'What will be your password?',
+      mask: '*',
+      default () {
+        return '123456'
+      },
+      validate (value) {
+        if (value.length < 6) {
+          return 'The password must be at least 6 characters'
+        }
+        Object.assign(newUser, { password: value })
+        return true
+      }
+    },
+    {
+      name: 'confirmation',
+      type: 'password',
+      message: 'Please, type your password again?',
+      mask: '*',
+      default () {
+        return '123456'
+      },
+      validate (value) {
+        if (value !== newUser.password) {
+          return 'The passwords do not match'
+        }
+        return true
+      }
+    }
+  ])
+  Object.assign(newUser, answers)
+}
+
 export default {
-  welcome
+  welcome,
+  menu,
+  register
 }
